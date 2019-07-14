8aeac3c5aa1fd8bdef18f34b84fe936298a1ad9f
christophwottawa <christoph.wottawa@me.com>
2019-07-14 11:48:24 +0200

    initial commit

---
+++ Algorithms/BinarySearch.swift
@@ -0,0 +1,10 @@
+//
+//  BinarySearch.swift
+//  Algorithms
+//
+//  Created by Christoph Wottawa on 14.07.19.
+//  Copyright © 2019 Christoph Wottawa. All rights reserved.
+//
+
+import Foundation
+
