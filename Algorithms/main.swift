b12960f64ef3bd8db041ab76196fefe1fb580dab
christophwottawa <christoph.wottawa@me.com>
2019-07-14 11:45:38 +0200

    Initial Commit

---
+++ Algorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithms
+//
+//  Created by Christoph Wottawa on 14.07.19.
+//  Copyright © 2019 Christoph Wottawa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
